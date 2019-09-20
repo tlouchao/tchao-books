63e06f24cf64aba838950db3da11654f69fa1117
Tiffany Chao <tiffanychao@Lucias-MacBook-Air-2.local>
2019-09-20 11:30:28 -0400

    disable cache, css in static directory

--- application.py
+++ application.py
@@ -1,12 +1,27 @@
 import os
 
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, url_for
 from flask_session import Session
 from functools import wraps
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import login_required
 
 app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -15,29 +30,37 @@ if not os.getenv("DATABASE_URL"):
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-# Login decorator
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if session.get("user_id") is None:
-            return redirect("/login")
-        return f(*args, **kwargs)
-    return decorated_function
 
-
+# Routing
 @app.route("/")
 @login_required
 def index():
-    res = request.get("https://www.goodreads.com/book/review_counts.json", params={"key": "IapbauVeI45oU7ttvyvg2Q", "isbns": "9780812524581"})
-    print(res.json())
-    return str(res.json())
+    return render_template("index.html")
+
+@app.route("/search", methods=["GET", "POST"])
+@login_required
+def search():
+    return render_template("search.html")
+
+@app.route("/review", methods=["GET", "POST"])
+@login_required
+def review():
+    return render_template("review.html")
+
+@app.route("/logout", methods=["GET", "POST"])
+@login_required
+def logout():
+    return render_template("logout.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
